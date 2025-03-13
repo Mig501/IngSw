f14d72891da7ab6f55425c7914f3ed67d9ee89ad
Paboct <pruizm01@estudiantes.unileon.es>
2025-03-13 13:31:16 +0100

    interfaz de prueba

--- proyecto_software/src/interface/Login.py
+++ proyecto_software/src/interface/Login.py
@@ -1,8 +1,8 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import uic
 
 # Cargar la interfaz generada desde el archivo .ui
-Form, Window = uic.loadUiType("./src/vista/ui/Login.ui")
+Form, Window = uic.loadUiType("./src/interface/ui/Login.ui")
 
 class MiVentana(QMainWindow, Form):
     def __init__(self):
@@ -10,13 +10,13 @@ class MiVentana(QMainWindow, Form):
         self.setupUi(self)  # Inicializa los widgets
 
         # Conectar el botón a la función
-        self.Boton.clicked.connect(self.on_button_click)
+        self.aceptar.clicked.connect(self.on_button_click) # Cualquier instancia de botton cuando se clica registra el evento
 
 
     def on_button_click(self):
         print("Botón presionado")
-        texto_area = self.Text.text() #Obtenet el texto del campo nombre
-        print("El texto es: ")
+        texto_area = self.nombre_usuario.text() #Obtener el texto del campo nombre
+        print("El user es: ", end="")
         print(texto_area)
 
 
@@ -24,4 +24,4 @@ if __name__ == "__main__":
     app = QApplication([])
     ventana = MiVentana()
     ventana.show()
-    app.exec_()
+    app.exec()

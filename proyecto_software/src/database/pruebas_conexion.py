b3f3447bdf5f52b70e7a8bc97524077ddc54aa5b
Paboct <pruizm01@estudiantes.unileon.es>
2025-03-27 12:52:14 +0100

    carpeta base de datos y prueba de conexion

---
+++ proyecto_software/src/database/pruebas_conexion.py
@@ -0,0 +1,9 @@
+import pymysql
+
+#conexion=mysql.connector.connect( host='localhost', user='root', password='changeme')
+conexion=pymysql.connect(host='localhost', user='root', password='changeme')
+cursor=conexion.cursor()
+cursor.execute("show databases")
+for base in cursor:
+    print(base)
+conexion.close()  

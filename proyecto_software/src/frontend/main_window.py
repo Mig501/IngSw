28372ebcf272503d956624a5c5557ee11df12607
blooraayy <jaime.alv.fdez@gmail.com>
2025-03-27 13:36:30 +0100

    Actualización frontend - Login

---
+++ proyecto_software/src/frontend/main_window.py
@@ -0,0 +1,19 @@
+from PyQt6.QtWidgets import QMainWindow, QStackedWidget
+from frontend.screens.login import LoginScreen
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("BSA App")
+        self.setGeometry(100, 100, 900, 600)
+
+        # Contenedor de pantallas
+        self.stack = QStackedWidget()
+        self.setCentralWidget(self.stack)
+
+        # Crear e insertar pantallas
+        self.login_screen = LoginScreen()
+        self.stack.addWidget(self.login_screen)  # index 0
+
+        # Mostrar login como pantalla inicial
+        self.stack.setCurrentWidget(self.login_screen)

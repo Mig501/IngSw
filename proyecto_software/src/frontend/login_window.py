348ac9ad7abf3819ec1504c82e1df04d5816f394
blooraayy <jaime.alv.fdez@gmail.com>
2025-04-03 13:57:27 +0200

    Cambios interfaz

--- proyecto_software/src/frontend/login_window.py
+++ proyecto_software/src/frontend/login_window.py
@@ -3,12 +3,14 @@
 from PyQt6.QtWidgets import QMainWindow
 from frontend.screens.login import LoginScreen
 from frontend.main_window import MainWindow
+from PyQt6.QtCore import Qt
 
 class LoginWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("BSA Login")
         self.setFixedSize(500, 600)
+        self.setWindowFlag(Qt.WindowType.WindowMaximizeButtonHint, False) # esto hay que ver como se hace para que funcione
 
         # Crear pantalla de login
         self.login_screen = LoginScreen()

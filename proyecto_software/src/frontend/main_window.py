5cef1951d094f023941920574eab5594c8479273
blooraayy <jaime.alv.fdez@gmail.com>
2025-04-03 13:41:14 +0200

    arreglo tras merge

--- proyecto_software/src/frontend/main_window.py
+++ proyecto_software/src/frontend/main_window.py
@@ -1,7 +1,7 @@
 # src/frontend/main_window.py
 
-from PyQt6.QtWidgets import QMainWindow, QStackedWidget
-from frontend.screens.login import LoginScreen
+from PyQt6.QtWidgets import QMainWindow, QLabel
+from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):

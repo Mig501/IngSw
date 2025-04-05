d9004556831737d0bf00aba9d20ebcb745ef07de
Mig501 <msancr11@estudiantes.unileon.es>
2025-04-05 19:24:38 +0200

    prueba login

--- proyecto_software/resources/database/BSA_Database_IngSW.sql
+++ proyecto_software/resources/database/BSA_Database_IngSW.sql
@@ -10,13 +10,13 @@ CREATE TABLE Users (
     Username VARCHAR(15) CHARACTER SET utf8mb4 NOT NULL UNIQUE,
     UserPassword VARCHAR(20) NOT NULL, 
     Email VARCHAR(90) CHARACTER SET utf8mb4 NOT NULL UNIQUE,
-    Add_Zip_Code VARCHAR(10),
+    /*Add_Zip_Code VARCHAR(10),
     Add_Country CHAR(3), 
     Add_Street VARCHAR(100) CHARACTER SET utf8mb4,
     Add_Number INT,
     Add_Floor_Num VARCHAR(5),
     Phone_Number CHAR(16),
-    Log_Date DATETIME DEFAULT NOW()
+    Log_Date DATETIME DEFAULT NOW()*/
     /*hay que añadir el campo para el rol de cada user para a la 
     de iniciar sesión, que pueda buscar en la base*/
 )

d9196c905f6fd3895dedadf97219709189641211
blooraayy <jaime.alv.fdez@gmail.com>
2025-05-09 19:42:46 +0200

    Barra de búsqueda por texto (interfaz)

--- proyecto_software/src/interface/windows/main_gui/screens/vehicle_screen.py
+++ proyecto_software/src/interface/windows/main_gui/screens/vehicle_screen.py
@@ -1,11 +1,23 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+# src/interface/windows/main_gui/screens/vehicle_screen.py
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit
 from PyQt6.QtCore import Qt
 
 class VehicleScreen(QWidget):
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
-        label = QLabel("Vehículos")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(label)
-        self.setLayout(layout)
+
+        # Layouts
+        self.layout = QVBoxLayout() # layout principal
+        self.upper_layout = QHBoxLayout() # layout para la zona superior de la barra de búsquedas y filtros
+
+        # Barra de búsqueda
+        self.search_bar = QLineEdit(self)
+        self.search_bar.setPlaceholderText("Buscar...")
+        self.search_bar.textChanged.connect(self.filter_results)
+
+        self.setLayout(self.layout)
+
+    def filter_results(self):
+        '''La función se activará cada vez que el texto cambie para actualizar la búsqueda'''
+        search_text = self.search_bar.text().lower()

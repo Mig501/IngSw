f16fe6af0180450e1721e8c8cd2e22a43a1d1108
blooraayy <jaime.alv.fdez@gmail.com>
2025-06-08 17:14:10 +0200

    Nueva imagen Home screen

--- proyecto_software/src/interface/windows/main_gui/screens/home_screen.py
+++ proyecto_software/src/interface/windows/main_gui/screens/home_screen.py
@@ -1,11 +1,22 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
 
 class HomeScreen(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        label = QLabel("Inicio")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(label)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Centrar contenido vertical y horizontalmente
+
+        # Imagen
+        self.image_label = QLabel()
+        pixmap = QPixmap("resources/images/logo2_removed.png")
+        if not pixmap.isNull():
+            pixmap = pixmap.scaled(400, 300, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+            self.image_label.setPixmap(pixmap)
+        else:
+            self.image_label.setText("Imagen no disponible")
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.image_label)
+
         self.setLayout(layout)
